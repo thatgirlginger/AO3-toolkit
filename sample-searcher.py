e204389b346014b1883442104bfffe17d8c0df6a
thatgirlginger <ginger.miller@gwu.edu>
2026-01-17 12:33:53 -0500

    revised search function and file writing, using the csv library rather than pandas to better handle pausing and resuming

--- sample-searcher.py
+++ sample-searcher.py
@@ -5,135 +5,103 @@ import pandas as pd
 import sys
 from getsearchdata import *
 
-url_orig = input("copy and paste the search url you are using")
+firsturl = "" #replace this with your starting url
 
-q = input("have you filtered your search? y or n") #if you just click on a tag without adding additional filters, there's a different url parameter
-
-date_update = []
-category = []
-warning = []
-completion = []
-rating = []
-fandom = []
-relationship = []
-character = []
-freeform = []
-words = []
-kudos = []
-hits = []
-language = []
-            
-
-start_page = int(input("what is your first page to grab?"))
-end_page = int(input("what is the last page to grab?"))
-
-
-for num in range(start_page, end_page + 1):
-    match q:
-        case "y":
-            url = url_orig + "&page=" + str(num)
-        case "n":
-            url = url_orig + "?page=" + str(num)
-    try:
-        response = requests.get(url, headers = {'user-agent': 'bot'})
-        html = response.content
-        soup = bs(html, 'html.parser')
-        work_container = soup.find("ol", class_="work index group")
-        try:
-            workitems = work_container.find_all('li', {'role':'article'})
-        except AttributeError:
-            print(f"attribute error at page {num}, should not be an issue") #when i do this i get random attribute errors, but it never actually causes an issue, and i haven't figured out why yet
-        print(f"page {num} has been souped, getting data now")
-
-    except requests.exceptions.HTTPError as err:
-        if err.response.status_code == 404:
-            print(f"404 Error at page {num}, see {url} for details")
-        elif err.response.status_code == 429:
-            print(f"too many requests error, take a minute and restart with page {num}") #if you get this, please be a good person and add a few seconds to your timing
-        a = input("do you want to continue making a csv with what has been scanned?")
-        match a:
-            case "y":
+def requestfunc(url, num):
+                url = f"{link}&page={num}"
+                try:
+                    response = requests.get(url, headers = {'user-agent': 'bot'})
+                    html = response.content
+                    soup = bs(html, 'html.parser')
+                    work_container = soup.find("ol", class_="work index group")
+                    workitems = work_container.find_all('li', {'role':'article'})
+                except AttributeError:
+                    pass
+                return workitems
+def makepagelist(startpage, endpage):
+            pagelist = []
+            for i in range (int(startpage), int(endpage) + 1):
+                        pagelist.append(i)
+            return pagelist
+def datafunc(num):
+            try:
+                workitems = requestfunc(url_orig, num)
+            except requests.exceptions.HTTPError as err:
+                if err.response.status_code == 404:
+                    print(f"404 Error at page {num}")
+                elif err.response.status_code == 429:
+                    print(f"too many requests error, take a minute and restart with page {num}") #if you get this, please be a good person and add a few seconds to your timing
                 break
-            case "n":
-                sys.exit()
+            print(f"page {num} has been souped")
+            for i in workitems:
+                try:
+                    work = workAttributes(i)
+                    datetime = work.getDateTime()
+                    fandom = work.getFandom()
 
-    for i in workitems:
+                    req = work.reqTagBlock()
+                    reqblock = requiredTags(req)
+
+                    rating = reqblock.getRating()
+                    warning = reqblock.getWarnings()
+
+                    tag = work.tagBlock()
+                    tagblock = tagAttributes(tag)
+
+                    relationship = tagblock.getRelationship()
+                    character = tagblock.getCharacters()
+                    freeform = tagblock.getFreeforms()
+
+                    stat = work.statsBlock()
+                    statblock = stats(stat)
+
+                    language = statblock.getLanguage()
+                    wordcount = statblock.getWords()
+                    kudos = statblock.getKudos()
+                    hits = statblock.getHits()
+                except AttributeError:
+                    pass
+                writer.writerow({'Date Last Updated':datetime, 'Warnings':warning, 'Ratings':rating, 'Fandom':fandom, 'Pairing':relationship, 'Character':character, 'Freeform':freeform, 'Word Count':wordcount, 'Kudos':kudos, 'Hits':hits, 'Language':language})
+            time.sleep(6)
+            print(f"page {num} has been added to your csv")
+
+def datafuncloop(url_orig, pagelist):
+    global pages_to_retry
+    pages_to_retry = []
+    for num in pagelist:
         try:
-            work = workAttributes(i)
+            datafunc(num)
+        except KeyboardInterrupt:
+            print(f"keyboard interrrupt at page {num}")
+            break
+        except Exception as e:
+            print(f"there's been another kind of exception: {e}\nadding {num} to list of pages to retry")
+            pages_to_retry.append(num)
 
-            datetimetag = work.getDateTime()
-            date_update.append(datetimetag)
+startpage = "" #set an integer for a startpage
+endpage = "" #same here
+pagelist = makepagelist(startpage, endpage)
 
-            fandomtag = work.getFandom()
-            fandom.append(fandomtag)
+filepath = "" #filepath here
+fieldnames = ['Date Last Updated', 'Warnings', 'Ratings', 'Fandom', 'Pairing', 'Character', 'Freeform', 'Word Count', 'Kudos', 'Hits', 'Language']
 
+if os.path.exists(filepath):
+            with open(filepath, 'a') as file:
+                        writer = csv.DictWriter(file, fieldnames = fieldnames)
+                        datafuncloop(firsturl, pagelist)
 
-            req = work.reqTagBlock()
-            reqblock = requiredTags(req)#you might be able to loop this bit, but i couldn't figure out the best way to
+else:
+            with open(filepath, 'w') as file:
+                        writer = csv.DictWriter(file, fieldnames = fieldnames)
+                        writer.writeheader()
+                        datafuncloop(firsturl, pagelist)
 
-            ratingtag = reqblock.getRating()
-            rating.append(ratingtag)
+#this next part will automatically go through and retry if there are error pages, i have not built it in yet to retry if they return an error again but i'm working on it
 
-            warningtag = reqblock.getWarnings()
-            warning.append(warningtag)
-
-            categorytag = reqblock.getCategory()
-            category.append(categorytag)
-
-            completiontag = reqblock.getCompletionStat()
-            completion.append(completiontag)
-
-
-            tag = work.tagBlock()
-            tagblock = tagAttributes(tag)
-
-            relationshiptag = tagblock.getRelationship()
-            relationship.append(relationshiptag)
-
-            charactertag = tagblock.getCharacters()
-            character.append(charactertag)
-
-            freeformtag = tagblock.getFreeforms()
-            freeform.append(freeformtag)
-
-            stat = work.statsBlock()
-            statblock = stats(stat)
-
-            lang = statblock.getLanguage()
-            language.append(lang)
-
-            wordcount = statblock.getWords()
-            words.append(wordcount)
-
-            kuds = statblock.getKudos()
-            kudos.append(kuds)
-
-            hit = statblock.getHits()
-            hits.append(hit)
-        except AttributeError:
-            pass
-
-    time.sleep(6)
-    print(f"page {num} has been added to your csv")
-
-data = {
-    "Date_Updated": pd.Series(date_update),
-    "Fandom": pd.Series(fandom),
-    "Category": pd.Series(category),
-    "Warnings" : pd.Series(warning),
-    "Completion Status": pd.Series(completion),
-    "Rating": pd.Series(rating),
-    "Pairing": pd.Series(relationship),
-    "Characters": pd.Series(character),
-    "Freeform": pd.Series(freeform),
-    "Word Count": pd.Series(words),
-    "Language": pd.Series(language),
-    "Kudos": pd.Series(kudos),
-    "Hits": pd.Series(hits)
-    }
-
-df = pd.DataFrame(data)
-print(f"a dataframe with {len(df)} rows has been created")
-
-#df.to_csv(path/to/your/new/csv)
-#print("go to your folder to see your new csv! bye now, i've sold you to one direction")
+count = len(pages_to_retry)
+if count > 0:
+            with open(filepath, 'a') as file:
+                        writer = csv.DictWriter(file, fieldnames = fieldnames)
+                        datafuncloop(firsturl, pages_to_retry)
+else:
+            print("finished! selling you to One Direction now")
