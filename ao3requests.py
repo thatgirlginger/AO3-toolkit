b6213f71a4a58a3e25c6febb006e838adda0a016
ging <ginger.miller@gwu.edu>
2026-02-16 16:40:27 -0500

    Add files via upload

---
+++ ao3requests.py
@@ -0,0 +1,25 @@
+import requests
+from bs4 import BeautifulSoup as bs
+
+
+class Request:
+    def __init__(self, url):
+        self.url = url
+    def objectify(self):
+        try:
+            response = requests.get(self.url, {'user-agent':'bot'})
+        except requests.exceptions.HTTPError as err:
+            print(f"error code f{err}")
+        html = response.content
+        soup = bs(html, 'html.parser')
+        return soup
+
+def paginate(object):
+    pagy = object.find('ol', class_='pagination actions pagy')
+    nextpage = pagy.find('li', class_='next').find('a')
+    if nextpage is not None:
+        nextlink = nextpage.get('href')
+        full = f"https://archiveofourown.org{nextlink}"
+        return full
+    else:
+        return None
---
+++ getworks.py
@@ -0,0 +1,94 @@
+from ao3funcs import *
+from ao3requests import *
+import csv
+import logging
+import os
+import pickle
+import sys
+from logsettings import setlog
+
+setlog()
+
+def midpointsave(x):
+    with open('pausedpage.pkl', 'wb') as f:
+            pickle.dump(x, f)    
+
+def getsearchdata(page):
+    logging.debug(f"log for scraping search {page}")
+    n=1
+    try:
+        while True:
+            if page == None:
+                break
+            else:
+                response = Request(page)
+                pageitem = response.objectify()
+                try:
+                    items = pageitem.find('ol', {'class':'work index group'})
+                    listings = items.find_all('li', {'role':'article'})
+                    p=0
+                    for i in listings:
+                        work = WorkAttributes(i)
+                        datetime = work.datetime()
+                        fandom = work.getfandom()
+
+                        requireds = RequiredTags(i)
+                        rating = requireds.getrating()
+                        warning = requireds.getwarnings()
+                        category = requireds.getcategory()
+
+                    
+                        attrs = TagAttributes(i)
+                        relationship = attrs.getrelationship()
+                        character = attrs.getcharacters()
+                        freeform = attrs.getfreeforms()
+
+                    
+                        stats = Stats(i)
+                        language = stats.getlanguage()
+                        wordcount = stats.getwords()
+                        kudos = stats.getkudos()
+                        hits = stats.gethits()
+                        chapters = stats.getchapters()
+                        writer.writerow({'Date Last Updated':datetime, 'Warnings':warning, 'Category':category, 'Ratings':rating, 'Fandom':fandom, 'Pairing':relationship, 'Character':character, 'Freeform':freeform, 'Word Count':wordcount, 'Kudos':kudos, 'Hits':hits, 'Language':language, 'Chapter Count':chapters})
+                        p+=1
+                        logging.DEBUG(f"work {p} of the page has been written to your csv")
+                except AttributeError:
+                    logging.warning("there's been an attribute error, ignoring for now but check csv to see what happened")
+                n+=1
+                logging.info(f"page {n} souped")
+            page = paginate(pageitem)
+    except KeyboardInterrupt:
+        midpointsave(page)
+        logging.info(f"interrupted by user at {n}, see log for details")
+        sys.exit()
+    except requests.exceptions.HTTPError as err:
+        logging.error(f"there's been an error getting the page: {err}")
+        midpointsave(page)
+        sys.exit()
+    except Exception as e:
+        logging.error(f"there's been an error at page {n}: {e}")
+        print("there's been an error, see log for details")
+        midpointsave(page)
+        sys.exit()
+
+
+url = "https://archiveofourown.org/tags/French%20Revolution/works" #eventually sys.argv[1]
+
+filepath = sys.argv[2]
+fieldnames = ['Date Last Updated', 'Warnings', 'Category', 'Ratings', 'Fandom', 'Pairing', 'Character', 'Freeform', 'Word Count', 'Kudos', 'Hits', 'Language', 'Chapter Count']
+
+fullfilepath = f"/Users/gingermaemiller/Desktop/ao3data/{filepath}.csv"
+
+if not os.path.exists(fullfilepath):
+    with open(fullfilepath, "w") as csvfile:
+        writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
+        writer.writeheader()
+        getsearchdata(page=url)
+else:
+    with open(fullfilepath, 'a') as csvfile:
+        with open('pausedpage.pkl', 'rb') as f:
+            page = pickle.load(f)
+        getsearchdata(page=page)
+
+#write function to read as a dataframe and drop duplicates
